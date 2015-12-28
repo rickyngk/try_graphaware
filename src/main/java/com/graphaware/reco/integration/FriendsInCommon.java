1dd57d8c54b6f118fcfde28935dff4b61b4d8a75
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2015-12-28 17:58:34 +0700

    fix test fail

--- src/main/java/com/graphaware/reco/integration/FriendsInCommon.java
+++ src/main/java/com/graphaware/reco/integration/FriendsInCommon.java
@@ -4,20 +4,26 @@ import com.graphaware.reco.generic.transform.ParetoScoreTransformer;
 import com.graphaware.reco.generic.transform.ScoreTransformer;
 import com.graphaware.reco.neo4j.engine.SomethingInCommon;
 import org.neo4j.graphdb.Direction;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.RelationshipType;
+
+import java.util.Collections;
+import java.util.Map;
 
 /**
  * Created by duynk on 12/27/15.
  */
 public class FriendsInCommon extends SomethingInCommon {
+    private ScoreTransformer scoreTransformer = new ParetoScoreTransformer(100, 10);
 
     public String name() {
         return "friendsInCommon";
     }
 
     @Override
-    protected Direction getDirection() {
-        return Direction.BOTH;
+    protected ScoreTransformer scoreTransformer() {
+        return scoreTransformer;
     }
 
     @Override
@@ -26,8 +32,13 @@ public class FriendsInCommon extends SomethingInCommon {
     }
 
     @Override
-    protected ScoreTransformer scoreTransformer() {
-        return new ParetoScoreTransformer(100, 10);
+    protected Direction getDirection() {
+        return Direction.BOTH;
+    }
+
+    @Override
+    protected Map<String, Object> details(Node thingInCommon, Relationship withInput, Relationship withOutput) {
+        return Collections.singletonMap("name", thingInCommon.getProperty("name"));
     }
 }
 
